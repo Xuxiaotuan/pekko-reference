f9438d094742bbcc65bc519af74634d5fcf1c712
Xuxiaotuan <jia_yangchen@163.com>
2023-08-27 17:37:53 +0800

    add Pekko Quickstart with Scala guide

---
+++ build.sbt
@@ -0,0 +1,20 @@
+name := "pekko-reference"
+
+version := "1.0"
+
+scalaVersion := "2.13.11"
+
+lazy val pekkoVersion = "1.0.1"
+
+// Run in a separate JVM, to make sure sbt waits until all threads have
+// finished before returning.
+// If you want to keep the application running while executing other
+// sbt tasks, consider https://github.com/spray/sbt-revolver/
+fork := true
+
+libraryDependencies ++= Seq(
+  "org.apache.pekko" %% "pekko-actor-typed" % pekkoVersion,
+  "ch.qos.logback" % "logback-classic" % "1.2.12",
+  "org.apache.pekko" %% "pekko-actor-testkit-typed" % pekkoVersion % Test,
+  "org.scalatest" %% "scalatest" % "3.2.15" % Test
+)
---
+++ src/main/scala/cn/xuyinyin/PekkoQuickstart.scala
@@ -0,0 +1,80 @@
+//#full-example
+package cn.xuyinyin
+
+
+import org.apache.pekko.actor.typed.ActorRef
+import org.apache.pekko.actor.typed.ActorSystem
+import org.apache.pekko.actor.typed.Behavior
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import cn.xuyinyin.GreeterMain.SayHello
+
+//#greeter-actor
+object Greeter {
+  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
+  final case class Greeted(whom: String, from: ActorRef[Greet])
+
+  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
+    context.log.info("Hello {}!", message.whom)
+    //#greeter-send-messages
+    message.replyTo ! Greeted(message.whom, context.self)
+    //#greeter-send-messages
+    Behaviors.same
+  }
+}
+//#greeter-actor
+
+//#greeter-bot
+object GreeterBot {
+
+  def apply(max: Int): Behavior[Greeter.Greeted] = {
+    bot(0, max)
+  }
+
+  private def bot(greetingCounter: Int, max: Int): Behavior[Greeter.Greeted] =
+    Behaviors.receive { (context, message) =>
+      val n = greetingCounter + 1
+      context.log.info("Greeting {} for {}", n, message.whom)
+      if (n == max) {
+        Behaviors.stopped
+      } else {
+        message.from ! Greeter.Greet(message.whom, context.self)
+        bot(n, max)
+      }
+    }
+}
+//#greeter-bot
+
+//#greeter-main
+object GreeterMain {
+
+  final case class SayHello(name: String)
+
+  def apply(): Behavior[SayHello] =
+    Behaviors.setup { context =>
+      //#create-actors
+      val greeter = context.spawn(Greeter(), "greeter")
+      //#create-actors
+
+      Behaviors.receiveMessage { message =>
+        //#create-actors
+        val replyTo = context.spawn(GreeterBot(max = 3), message.name)
+        //#create-actors
+        greeter ! Greeter.Greet(message.name, replyTo)
+        Behaviors.same
+      }
+    }
+}
+//#greeter-main
+
+//#main-class
+object PekkoQuickstart extends App {
+  //#actor-system
+  val greeterMain: ActorSystem[GreeterMain.SayHello] = ActorSystem(GreeterMain(), "PekkoQuickstart")
+  //#actor-system
+
+  //#main-send-messages
+  greeterMain ! SayHello("Charles")
+  //#main-send-messages
+}
+//#main-class
+//#full-example
